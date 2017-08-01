53e979b5a12aef20e293bb9eb90c342027a79903
Jaruzel <github@jaruzel.com>
2017-08-01 12:44:01 +0100

    Initial Commit

---
+++ Source/JoypadTester/frmMain.Designer.vb
@@ -0,0 +1,114 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmMain
+  Inherits System.Windows.Forms.Form
+
+  'Form overrides dispose to clean up the component list.
+  <System.Diagnostics.DebuggerNonUserCode()> _
+  Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+    Try
+      If disposing AndAlso components IsNot Nothing Then
+        components.Dispose()
+      End If
+    Finally
+      MyBase.Dispose(disposing)
+    End Try
+  End Sub
+
+  'Required by the Windows Form Designer
+  Private components As System.ComponentModel.IContainer
+
+  'NOTE: The following procedure is required by the Windows Form Designer
+  'It can be modified using the Windows Form Designer.  
+  'Do not modify it using the code editor.
+  <System.Diagnostics.DebuggerStepThrough()> _
+  Private Sub InitializeComponent()
+    Me.components = New System.ComponentModel.Container
+    Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(frmMain))
+    Me.btnSelect = New System.Windows.Forms.Button
+    Me.txtDir = New System.Windows.Forms.TextBox
+    Me.txtButtons = New System.Windows.Forms.TextBox
+    Me.timerPoll = New System.Windows.Forms.Timer(Me.components)
+    Me.ComboBox1 = New System.Windows.Forms.ComboBox
+    Me.btnScan = New System.Windows.Forms.Button
+    Me.SuspendLayout()
+    '
+    'btnSelect
+    '
+    Me.btnSelect.Font = New System.Drawing.Font("Segoe UI", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+    Me.btnSelect.Location = New System.Drawing.Point(653, 11)
+    Me.btnSelect.Name = "btnSelect"
+    Me.btnSelect.Size = New System.Drawing.Size(99, 27)
+    Me.btnSelect.TabIndex = 0
+    Me.btnSelect.Text = "Select Device"
+    Me.btnSelect.UseVisualStyleBackColor = True
+    '
+    'txtDir
+    '
+    Me.txtDir.Font = New System.Drawing.Font("Segoe UI", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+    Me.txtDir.Location = New System.Drawing.Point(12, 55)
+    Me.txtDir.Multiline = True
+    Me.txtDir.Name = "txtDir"
+    Me.txtDir.Size = New System.Drawing.Size(740, 534)
+    Me.txtDir.TabIndex = 3
+    '
+    'txtButtons
+    '
+    Me.txtButtons.Font = New System.Drawing.Font("Segoe UI", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+    Me.txtButtons.Location = New System.Drawing.Point(758, 55)
+    Me.txtButtons.Multiline = True
+    Me.txtButtons.Name = "txtButtons"
+    Me.txtButtons.ScrollBars = System.Windows.Forms.ScrollBars.Vertical
+    Me.txtButtons.Size = New System.Drawing.Size(184, 534)
+    Me.txtButtons.TabIndex = 4
+    '
+    'timerPoll
+    '
+    '
+    'ComboBox1
+    '
+    Me.ComboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
+    Me.ComboBox1.Font = New System.Drawing.Font("Segoe UI", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+    Me.ComboBox1.FormattingEnabled = True
+    Me.ComboBox1.Location = New System.Drawing.Point(12, 12)
+    Me.ComboBox1.Name = "ComboBox1"
+    Me.ComboBox1.Size = New System.Drawing.Size(597, 25)
+    Me.ComboBox1.TabIndex = 5
+    '
+    'btnScan
+    '
+    Me.btnScan.Font = New System.Drawing.Font("Segoe UI", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+    Me.btnScan.Location = New System.Drawing.Point(843, 11)
+    Me.btnScan.Name = "btnScan"
+    Me.btnScan.Size = New System.Drawing.Size(99, 27)
+    Me.btnScan.TabIndex = 6
+    Me.btnScan.Text = "Device Scan"
+    Me.btnScan.UseVisualStyleBackColor = True
+    '
+    'frmMain
+    '
+    Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+    Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+    Me.ClientSize = New System.Drawing.Size(954, 601)
+    Me.Controls.Add(Me.btnScan)
+    Me.Controls.Add(Me.ComboBox1)
+    Me.Controls.Add(Me.txtButtons)
+    Me.Controls.Add(Me.txtDir)
+    Me.Controls.Add(Me.btnSelect)
+    Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+    Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+    Me.MaximizeBox = False
+    Me.Name = "frmMain"
+    Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+    Me.Text = "Joypad / Joystick Tester - github.com/Jaruzel"
+    Me.ResumeLayout(False)
+    Me.PerformLayout()
+
+  End Sub
+  Friend WithEvents btnSelect As System.Windows.Forms.Button
+  Friend WithEvents txtDir As System.Windows.Forms.TextBox
+  Friend WithEvents txtButtons As System.Windows.Forms.TextBox
+  Friend WithEvents timerPoll As System.Windows.Forms.Timer
+  Friend WithEvents ComboBox1 As System.Windows.Forms.ComboBox
+  Friend WithEvents btnScan As System.Windows.Forms.Button
+
+End Class
---
+++ Source/JoypadTester/frmMain.vb
@@ -0,0 +1,101 @@
+﻿Imports Microsoft.DirectX.DirectInput
+Imports System.Threading
+Public Class frmMain
+
+  Public objDevice As Device
+  Private Sub frmMain_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+    ScanForDevices()
+  End Sub
+  Private Sub btnSelect_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnSelect.Click
+    SelectDevice()
+  End Sub
+  Private Sub btnScan_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnScan.Click
+    ScanForDevices()
+  End Sub
+  Private Sub timerPoll_Tick(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles timerPoll.Tick
+    joystickPolling()
+  End Sub
+  Sub ScanForDevices()
+    Dim gameControllerList As DeviceList
+    gameControllerList = Manager.GetDevices(DeviceClass.GameControl, EnumDevicesFlags.AttachedOnly)
+
+    ComboBox1.Items.Clear()
+
+    If (gameControllerList.Count > 0) Then
+
+      Dim deviceInstance As DeviceInstance
+      For Each deviceInstance In gameControllerList
+        ComboBox1.Items.Add("{" & deviceInstance.InstanceGuid.ToString & "} - " & deviceInstance.InstanceName)
+      Next
+      ComboBox1.SelectedIndex = 0
+    End If
+    timerPoll.Enabled = False
+    txtDir.Clear()
+    txtButtons.Clear()
+  End Sub
+  Sub SelectDevice()
+    Dim strDeviceID As String = ComboBox1.SelectedItem
+    strDeviceID = strDeviceID.Substring(1, strDeviceID.IndexOf("}") - 1)
+
+    Dim gameControllerList As DeviceList
+    gameControllerList = Manager.GetDevices(DeviceClass.GameControl, EnumDevicesFlags.AttachedOnly)
+
+    Dim deviceInstance As DeviceInstance
+    For Each deviceInstance In gameControllerList
+      If deviceInstance.InstanceGuid.ToString = strDeviceID Then
+        objDevice = New Device(deviceInstance.InstanceGuid)
+        objDevice.SetDataFormat(DeviceDataFormat.Joystick)
+        objDevice.Acquire()
+        timerPoll.Enabled = True
+        timerPoll.Start()
+        Exit For
+      End If
+    Next
+
+  End Sub
+  Private Sub joystickPolling()
+    Dim strData As String = ""
+
+    Try
+      objDevice.Poll()
+      Dim state As JoystickState = objDevice.CurrentJoystickState
+      strData = strData & state.ARx & vbTab & "ARx" & vbTab & "X-axis angular acceleration. " & vbCrLf
+      strData = strData & state.ARy & vbTab & "ARy" & vbTab & "Y-axis angular acceleration. " & vbCrLf
+      strData = strData & state.ARz & vbTab & "ARz" & vbTab & "Z-axis angular acceleration. " & vbCrLf
+      strData = strData & state.AX & vbTab & "AX" & vbTab & "X-axis acceleration. " & vbCrLf
+      strData = strData & state.AY & vbTab & "AY" & vbTab & "Y-axis acceleration. " & vbCrLf
+      strData = strData & state.AZ & vbTab & "AZ" & vbTab & "Z-axis acceleration. " & vbCrLf
+      strData = strData & state.FRx & vbTab & "FRx" & vbTab & "X-axis torque. " & vbCrLf
+      strData = strData & state.FRy & vbTab & "FRy" & vbTab & "Y-axis torque. " & vbCrLf
+      strData = strData & state.FRz & vbTab & "FRz" & vbTab & "Z-axis torque. " & vbCrLf
+      strData = strData & state.FX & vbTab & "FX" & vbTab & "X-axis force. " & vbCrLf
+      strData = strData & state.FY & vbTab & "FY" & vbTab & "Y-axis force. " & vbCrLf
+      strData = strData & state.FZ & vbTab & "FZ" & vbTab & "Z-axis force. " & vbCrLf
+      strData = strData & state.Rx & vbTab & "Rx" & vbTab & "X-axis rotation. If the joystick does not have this axis, the value is 0. " & vbCrLf
+      strData = strData & state.Ry & vbTab & "Ry" & vbTab & "Y-axis rotation. If the joystick does not have this axis, the value is 0. " & vbCrLf
+      strData = strData & state.Rz & vbTab & "Rz" & vbTab & "Z-axis rotation (often called the rudder). If the joystick does not have this axis, the value is 0. " & vbCrLf
+      strData = strData & state.VRx & vbTab & "VRx" & vbTab & "X-axis angular velocity. " & vbCrLf
+      strData = strData & state.VRy & vbTab & "VRy" & vbTab & "Y-axis angular velocity. " & vbCrLf
+      strData = strData & state.VRz & vbTab & "VRz" & vbTab & "Z-axis angular velocity. " & vbCrLf
+      strData = strData & state.VX & vbTab & "VX" & vbTab & "X-axis velocity. " & vbCrLf
+      strData = strData & state.VY & vbTab & "VY" & vbTab & "Y-axis velocity. " & vbCrLf
+      strData = strData & state.VZ & vbTab & "VZ" & vbTab & "Z-axis velocity. " & vbCrLf
+      strData = strData & state.X & vbTab & "X" & vbTab & "X-axis, usually the left-right movement of a stick. " & vbCrLf
+      strData = strData & state.Y & vbTab & "Y" & vbTab & "Y-axis, usually the forward-backward movement of a stick. " & vbCrLf
+      strData = strData & state.Z & vbTab & "Z" & vbTab & "Z-axis, often the throttle control. If the joystick does not have this axis, the value is 0. " & vbCrLf
+
+      If txtDir.Text <> strData Then txtDir.Text = strData
+
+      strData = ""
+      For iButton As Integer = 0 To objDevice.Caps.NumberButtons - 1
+        strData = strData & "Button #" & iButton & ":" & vbTab & state.GetButtons(iButton) & vbCrLf
+      Next
+
+      If txtButtons.Text <> strData Then txtButtons.Text = strData
+
+    Catch ex As Exception
+
+    End Try
+  End Sub
+
+End Class
